d4518fd7ecd009537d8bf13d5248add77d808685
Hamzathvanan <hamzathvanan@gmail.com>
2024-09-01 01:02:25 +0530

    Initial commit - ABC Restaurant Project

---
+++ ABCRestaurant/ABCWebParent/ABCBackEnd/src/main/java/com/abcrestaurant/admin/AbcBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.abcrestaurant.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcBackEndApplication.class, args);
+	}
+
+}
