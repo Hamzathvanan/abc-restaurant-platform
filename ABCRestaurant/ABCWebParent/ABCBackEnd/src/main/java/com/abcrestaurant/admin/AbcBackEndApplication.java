8f31b2a01ea948867bbdf924fe4d09d98063a135
Hamzathvanan <hamzathvanan@gmail.com>
2024-09-03 04:46:48 +0530

    Database and Configure Data Source, Entity, Role Repository and Unit Test

--- ABCRestaurant/ABCWebParent/ABCBackEnd/src/main/java/com/abcrestaurant/admin/AbcBackEndApplication.java
+++ ABCRestaurant/ABCWebParent/ABCBackEnd/src/main/java/com/abcrestaurant/admin/AbcBackEndApplication.java
@@ -2,8 +2,10 @@ package com.abcrestaurant.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.abcrestaurant.common.entity", "com.abcrestaurant.admin.user"})
 public class AbcBackEndApplication {
 
 	public static void main(String[] args) {
