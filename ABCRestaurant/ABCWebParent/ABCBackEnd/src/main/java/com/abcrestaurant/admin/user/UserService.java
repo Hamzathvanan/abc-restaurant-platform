9b890f7c3770207cc185786a757f0f01f2b0c543
Hamzathvanan <hamzathvanan@gmail.com>
2024-09-05 02:39:20 +0530

    User Password Encode

--- ABCRestaurant/ABCWebParent/ABCBackEnd/src/main/java/com/abcrestaurant/admin/user/UserService.java
+++ ABCRestaurant/ABCWebParent/ABCBackEnd/src/main/java/com/abcrestaurant/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.abcrestaurant.admin.user;
 import com.abcrestaurant.common.entity.Role;
 import com.abcrestaurant.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +17,9 @@ public class UserService {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
     }
@@ -25,7 +29,13 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepository.save(user);
+    }
+
+    private void encodePassword(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
 
