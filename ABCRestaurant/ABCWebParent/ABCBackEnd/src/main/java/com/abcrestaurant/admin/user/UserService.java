d15d927f88420829b03b0e02ad11426e3b378648
Hamzathvanan <hamzathvanan@gmail.com>
2024-09-05 01:39:22 +0530

    Create New User Functions

---
+++ ABCRestaurant/ABCWebParent/ABCBackEnd/src/main/java/com/abcrestaurant/admin/user/UserService.java
@@ -0,0 +1,31 @@
+package com.abcrestaurant.admin.user;
+
+import com.abcrestaurant.common.entity.Role;
+import com.abcrestaurant.common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public List<User> listAll() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+}
+
