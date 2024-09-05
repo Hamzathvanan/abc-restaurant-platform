988d711cb76f0f5266919c4b060a407c00d9f05c
Hamzathvanan <hamzathvanan@gmail.com>
2024-09-06 01:37:07 +0530

    Update User Functions

---
+++ ABCRestaurant/ABCWebParent/ABCBackEnd/src/main/java/com/abcrestaurant/admin/user/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.abcrestaurant.admin.user;
+
+public class UserNotFoundException extends Throwable {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
